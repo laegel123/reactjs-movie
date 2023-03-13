e2b296d69d16338884aba6ee3adf01ac64f86350
laegel <laegel1@gmail.com>
2023-03-13 15:14:07 +0900

    Title : movie 프로젝트 api 연동

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import {useEffect, useState} from "react";
+import Movie from "./Movie";
 
-function App() {
+
+function App(callbackfn, thisArg) {
     const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(
+            'https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year'
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    }
 
     useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json) => {
-                setCoins(json);
-                setLoading(false);
-            });
-    }, [])
+        getMovies();
+    }, []);
 
     return (
         <div>
-            <h1>The Coins ! ({coins.length})</h1>
-            {loading ? <strong>Loading...</strong> : null}
-            <ul>
-                {coins.map(coin => <li>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</li>)}
-
-            </ul>
+            {loading ? <h1>Loading ... </h1> :
+                <div>
+                    {movies.map(movie =>
+                        <Movie
+                            key={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+                        />
+                    )}
+                </div>
+            }
         </div>
     );
 }
